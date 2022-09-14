94240151a0b47549c2a2686d6c30fb5c43f0283f
Ian12467 <64345032+Ian12467@users.noreply.github.com>
2022-09-14 22:22:15 +0300

    update

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,59 +1,21 @@
 #!/usr/bin/python3
-"""defines a class called square"""
+"""Magic class from a given ByteCode."""
+import math
 
 
-class Square:
-    """Initialize a square
-    Args:
-        __size (int): size of the square
-    """
-    def __init__(self, size=0):
-        """Initialize a square
-        Args:
-            size (int): size of the square
-        Returns: None
-        """
-        self.size = size
-
-    @property
-    def size(self):
-        """getter of the size"""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """sets the value for size"""
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
-        else:
-            raise TypeError("size must be an integer")
+class MagicClass:
+    """Initialization of the MagicClass."""
+    def __init__(self, radius=0):
+        """Initialization of the data."""
+        self._MagicClass__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self._MagicClass__radius = radius
 
     def area(self):
-        """This computes the area of the square"""
-        return (self.__size ** 2)
+        """Calculation of the area."""
+        return self._MagicClass__radius ** 2 * math.pi
 
-    def __eq__(self, other):
-        """equality representation"""
-        return self.__size == other.__size
-
-    def __ne__(self, other):
-        """not equal to representation"""
-        return self.__size != other.__size
-
-    def __gt__(self, other):
-        """greater thanepresentation"""
-        return self.__size > other.__size
-
-    def __ge__(self, other):
-        """greater than or equal to representation"""
-        return self.__size >= other.__size
-
-    def __lt__(self, other):
-        """less than reprsentation"""
-        return self.__size < other.__size
-
-    def __le__(self, other):
-        """less than or equal to representation"""
-        return self.__size <= other.__size
+    def circumference(self):
+        """Calculation of the circumference."""
+        return 2 * math.pi * self._MagicClass__radius
