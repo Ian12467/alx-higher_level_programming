6b4faae026a5d217aeec41c9acdadde9db23b896
Ian12467 <64345032+Ian12467@users.noreply.github.com>
2022-09-27 22:32:49 +0300

    add:12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""Module 14-pascal_triangle.
-Returns a list of lists of integers
-representing the Pascal’s triangle of n.
+"""
+File: 12-pascal_triangle.py
+Desc: This module contains one function; pascal_triangle(n)
+Author: Ian Otieno (Ian12467)
+Date Created: 26 Sep 2022
 """
 
 
 def pascal_triangle(n):
-    """Returns the pascal triangle of n.
-
-    Args:
-        - n: size of the triangle (rows)
-
-    Returns: a list of list of integers
     """
-
+    Returns a list of lists of integers representing the
+    Pascal’s triangle of n
+    """
     if n <= 0:
         return []
-
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
-
-    for i in range(1, n):
-        l[i][0] = 1
-        for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
-            else:
-                l[i][j] = l[i - 1][0]
-    return l
+    triangle = [[1]]
+    while len(triangle) != n:
+        hold = triangle[-1]
+        inside = [1]
+        for i in range(len(hold) - 1):
+            inside.append(hold[i] + hold[i + 1])
+        inside.append(1)
+        triangle.append(inside)
+    return triangle
